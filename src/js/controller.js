7e31f136014b5c4432237311e287d2111d0c5cb4
Sondos Amr <sondos.amr444@gmail.com>
2025-12-30 20:01:45 +0200

    Refactor recipe object structure

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,39 +1,30 @@
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-const showRecipe = async function () {
+const showRecipe = async function (id) {
   try {
-    const res =
-      await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=pizza/5ed6604591c37cdc054bc886
-`);
-    const data = await res.json();
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+    );
 
+    const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-    console.log(res, data);
     let { recipe } = data.data;
 
     recipe = {
       id: recipe.id,
       title: recipe.title,
       publisher: recipe.publisher,
-      sourcerUrl: recipe.sourcl_url,
+      sourceUrl: recipe.source_url,
       image: recipe.image_url,
       servings: recipe.servings,
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
     console.log(recipe);
+    console.log(data);
   } catch (err) {
     alert(err);
   }
 };
 
-showRecipe();
+showRecipe('5ed6604591c37cdc054bc886');
