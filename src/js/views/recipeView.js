87569e9a4fd21c885c020063e954d127e81c3d0d
Sondos Amr <sondos.amr444@gmail.com>
2026-01-06 12:24:04 +0200

    Update libraries: use fracty for ingredient fractions and fix icons path

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,6 @@
+import fracty from 'fracty';
 import icons from 'url:../../img/icons.svg';
+console.log(fracty);
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
@@ -76,7 +78,9 @@ class RecipeView {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
+              <div class="recipe__quantity">${
+                ing.quantity ? fracty(ing.quantity).toString() : ''
+              }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
