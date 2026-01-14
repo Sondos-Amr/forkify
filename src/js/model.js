87d6ce8c7bc846bb692a15793b836e8db56981c5
Sondos Amr <sondos.amr444@gmail.com>
2026-01-14 20:06:46 +0200

    Add search state and implement loadSearchResults in model

--- src/js/model.js
+++ src/js/model.js
@@ -1,26 +1,30 @@
 import { API_URL } from './config';
 import { getJSON } from './helpers';
 export const state = {
-  recipe: {},
+  res: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
-export const loadRecipe = async function (id) {
+export const loadres = async function (id) {
   try {
     const data = await getJSON(`${API_URL}${id}`);
-    const { recipe } = data.data;
+    const { res } = data.data;
 
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
+    state.res = {
+      id: res.id,
+      title: res.title,
+      publisher: res.publisher,
+      sourceUrl: res.source_url,
+      image: res.image_url,
+      servings: res.servings,
+      cookingTime: res.cooking_time,
+      ingredients: res.ingredients,
     };
 
-    console.log(state.recipe);
+    console.log(state.res);
     console.log(data);
   } catch (err) {
     console.error(`${err} 🚨🚨🚨`);
@@ -29,9 +33,21 @@ export const loadRecipe = async function (id) {
 };
 
 export const loadSearchResults = async function (query) {
+  state.search.query = query;
   try {
     const data = await getJSON(`${API_URL}?search=${query}`);
     console.log('dataaaa', data);
+
+    state.search.results = data.data.recipes.map(res => {
+      return {
+        id: res.id,
+        title: res.title,
+        publisher: res.publisher,
+        image: res.image_url,
+      };
+    });
+    console.log('query : ', state.search.query);
+    console.log('results : ', state.search.results);
   } catch (err) {
     console.error(`${err} 🚨🚨🚨`);
     throw err;
