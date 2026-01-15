fadbe117f95eaed0401c08db762d534af30c72a6
Sondos Amr <sondos.amr444@gmail.com>
2026-01-15 07:00:37 +0200

    Fix result preview links to use hash-based routing

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -9,7 +9,7 @@ class ResultView extends View {
   _generateMarkupPreview(result) {
     return `
           <li class="preview">
-            <a class="preview__link preview__link--active" href="${result.id}">
+            <a class="preview__link preview__link--active" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="Test" />
               </figure>
