21a74eac470a9511d7ee92a64e49f5bd9df46de5
Sondos Amr <sondos.amr444@gmail.com>
2026-01-27 23:02:41 +0200

    Add pagination: set resultsPerPage and create getSearchResultsPage to slice search results per page

--- src/js/model.js
+++ src/js/model.js
@@ -1,10 +1,12 @@
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
+
 export const state = {
   res: {},
   search: {
     query: '',
     results: [],
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -47,4 +49,10 @@ export const loadSearchResults = async function (query) {
     console.error(`${err} 🚨🚨🚨`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page) {
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
 };
