48f1f946640907dc2aa6ed26e952adb6ed521bae
Sondos Amr <sondos.amr444@gmail.com>
2026-01-27 23:20:03 +0200

    Add current page state and default parameter for getSearchResultsPage

--- src/js/model.js
+++ src/js/model.js
@@ -7,6 +7,7 @@ export const state = {
     query: '',
     results: [],
     resultsPerPage: RES_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -51,7 +52,8 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-export const getSearchResultsPage = function (page) {
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
   const start = (page - 1) * state.search.resultsPerPage;
   const end = page * state.search.resultsPerPage;
   return state.search.results.slice(start, end);
