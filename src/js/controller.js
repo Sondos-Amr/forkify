f951eea6fa29b02bde5e78bb29ef12c62cf6bee9
Sondos_Amr <sondos.amr444@gmail.com>
2026-06-16 11:54:43 +0300

    implement recipe loading using URL hash and hashchange event

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,11 +27,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log('id', id);
     // 1- Loading recipe
 
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886`,
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
     console.log(res, data);
@@ -148,4 +150,6 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+// showRecipe();
+
+window.addEventListener('hashchange', showRecipe);
